7160b157fed511c75227748f2c083ac0b07224f7
Abraham Anaba <anabangonoam@gmail.com>
2024-12-03 11:56:28 +0100

    Retrait de suspend dans dans la declaration des requetes

--- app/src/main/java/com/example/fliesbook/data/dao/AirportDao.kt
+++ app/src/main/java/com/example/fliesbook/data/dao/AirportDao.kt
@@ -8,12 +8,15 @@ import com.example.fliesbook.data.model.Airport
 @Dao
 interface AirportDao {
 
+    // Insérer un nouvel aéroport dans la base de données
     @Insert
-    suspend fun insertAirport(airport: Airport)
+    fun insertAirport(airport: Airport)
 
+    // Rechercher des aéroports dont le code IATA correspond à la requête fournie
     @Query("SELECT * FROM airport WHERE iata_code LIKE :iataCode LIMIT 10")
-    suspend fun searchAirportsByCode(iataCode: String): List<Airport>
+    fun searchAirportsByCode(iataCode: String): List<Airport>
 
+    // Récupérer un aéroport spécifique par son ID
     @Query("SELECT * FROM airport WHERE id = :id")
-    suspend fun getAirportById(id: Int): Airport
+    fun getAirportById(id: Int): Airport
 }
